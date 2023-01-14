5151610f8b10c3161b3f37edb52258738bae7db1
AI悦创 <cleland1432803776@icloud.com>
2023-01-14 19:29:40 +0800

    update

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ URL = 'https://github.com/AndersonHJB/EasyDB'
 EMAIL = 'bornforthis@bornforthis.cn'
 AUTHOR = 'Bornforthis'
 REQUIRES_PYTHON = '>=3.6.0'
-VERSION = '0.0.3'
+VERSION = '0.0.4'
 
 # What packages are required for this module to be executed?
 REQUIRED = [
